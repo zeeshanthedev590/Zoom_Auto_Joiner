9c8946d52a4e5181f27f02bf167698b658b2602c
zeeshanthedev590 <zeeshanthedev@gmail.com>
2020-12-17 15:07:52 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyautogui
+pyautogui.FAILSAFE = False
+pyautogui.click(x=708, y=744, button='left', clicks=1, interval=0.5)
+pyautogui.click(x=532, y=291, button='left', clicks=1, interval=0.5)
+pyautogui.click(x=576, y=327, button='left', clicks=1, interval=0.5)
+pyautogui.write('73860885849')
+pyautogui.click(x=530, y=457, button='left', clicks=1, interval=0.5)
+pyautogui.click(x=714, y=488, button='left', clicks=1, interval=0.5)
+pyautogui.click(x=665, y=334, button='left', clicks=1, interval=4)
+pyautogui.write('class8')
+pyautogui.click(x=714, y=488, button='left', clicks=1, interval=3)

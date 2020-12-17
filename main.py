e0f4fc5e8344c6047db2bf867c11ff39a0b735cb
zeeshanthedev590 <zeeshanthedev@gmail.com>
2020-12-17 17:16:16 +0500

    Complete

--- main.py
+++ main.py
@@ -1,11 +1,56 @@
 import pyautogui
+import os
+import time
 pyautogui.FAILSAFE = False
-pyautogui.click(x=708, y=744, button='left', clicks=1, interval=0.5)
-pyautogui.click(x=532, y=291, button='left', clicks=1, interval=0.5)
-pyautogui.click(x=576, y=327, button='left', clicks=1, interval=0.5)
-pyautogui.write('73860885849')
-pyautogui.click(x=530, y=457, button='left', clicks=1, interval=0.5)
-pyautogui.click(x=714, y=488, button='left', clicks=1, interval=0.5)
-pyautogui.click(x=665, y=334, button='left', clicks=1, interval=4)
-pyautogui.write('class8')
-pyautogui.click(x=714, y=488, button='left', clicks=1, interval=3)
+id = input('What Is Your Meeting Id:')
+passcode = input('What Is Your Passcode:')
+os.startfile('C://Users//Zeeshan Khalid//AppData//Roaming//Zoom//bin//zoom.exe')
+time.sleep(3)
+
+join = pyautogui.locateOnScreen(
+    'G://My Projects//Python Projects//Zoom_Auto_Joiner//join.png')
+print(join)
+pyautogui.moveTo(join)
+pyautogui.click(join)
+
+time.sleep(2)
+id_in = pyautogui.locateOnScreen(
+    'G://My Projects//Python Projects//Zoom_Auto_Joiner//id.png')
+
+print(id_in)
+pyautogui.moveTo(id_in)
+pyautogui.click(id_in)
+pyautogui.write(id)
+
+time.sleep(2)
+video = pyautogui.locateOnScreen(
+    'G://My Projects//Python Projects//Zoom_Auto_Joiner//video.png')
+
+print(video)
+pyautogui.moveTo(video)
+pyautogui.click(video)
+
+time.sleep(2)
+btn1 = pyautogui.locateOnScreen(
+    'G://My Projects//Python Projects//Zoom_Auto_Joiner//btn1.png')
+
+print(btn1)
+pyautogui.moveTo(btn1)
+pyautogui.click(btn1)
+
+time.sleep(2)
+passw = pyautogui.locateOnScreen(
+    'G://My Projects//Python Projects//Zoom_Auto_Joiner//pass.png')
+
+print(passw)
+pyautogui.moveTo(passw)
+pyautogui.click(passw)
+pyautogui.write(passcode)
+
+time.sleep(2)
+btn2 = pyautogui.locateOnScreen(
+    'G://My Projects//Python Projects//Zoom_Auto_Joiner//btn2.png')
+
+print(btn2)
+pyautogui.moveTo(btn2)
+pyautogui.click(btn2)
